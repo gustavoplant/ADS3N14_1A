bd74fbf36fbe7f5de091d887d4957a125b184526
Gustavo Araujo <gustavo.plant@gmail.com>
2014-04-06 00:25:31 -0300

    movido codigo do insertSort para a classe propria

--- trabalho_4/src/InsertSort.java
+++ trabalho_4/src/InsertSort.java
@@ -1,4 +1,30 @@
 
 public class InsertSort {
 
+	public int[] insertSort (int[] desNros){
+		
+		int i,c,j,cont = 0;
+		int [] retArr = new int[desNros.length + 1];
+		
+		for (i=1;i< desNros.length;i++){
+			c = desNros[i];
+			j = i;
+			
+			while (j > 0 && desNros[j-1] > c){
+				cont++;
+				desNros[j] = desNros[j-1];
+				j = j - 1;
+				desNros[j] = c;
+			}
+		}
+		
+		for (i=0;i<desNros.length;i++){
+			retArr[i] = desNros[i];
+		}
+				
+		retArr[desNros.length] = cont;
+		
+		return retArr;
+	}
+	
 }
